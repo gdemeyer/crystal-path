cff3a576d54e6103b4eed75e965c5b716f0d73fe
Garrett DeMeyer <gddemeyer@gmail.com>
2025-05-01 18:03:10 -0500

    add algo

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,48 @@ import TaskEntryBlock from './components/taskEntryBlock.tsx';
 import './App.css';
 import TaskSummaryCard from './components/taskSummaryCard.tsx';
 import TaskSummaryCardContainer from './components/taskSummaryCardBlock.tsx';
+import { Task } from './types/types.ts';
+import sortTasks from './utils/algo.ts';
+
+const tasks: Task[] = [
+  {
+    title: "Play Card Games with Aurora",
+    urgency: 13,
+    impact: 13,
+    time: 5,
+    difficulty: 8,
+  },
+  {
+    title: "Sleep",
+    urgency: 3,
+    impact: 5,
+    time: 8,
+    difficulty: 1,
+  },
+  {
+    title: "A Secret Third Thing",
+    urgency: 13,
+    impact: 13,
+    time: 1,
+    difficulty: 1,
+  },
+  {
+    title: "Profit",
+    urgency: 1,
+    impact: 1,
+    time: 13,
+    difficulty: 13,
+  },
+]
+
+const sortedTasks = sortTasks(tasks)
 
 function App() {
   return (
     <div className="App">
       <TaskEntryBlock />
       <TaskSummaryCardContainer>
-        <TaskSummaryCard title="Taxes" />
-        <TaskSummaryCard title="Sleep" />
-        <TaskSummaryCard title="A secret third thing" />
-        <TaskSummaryCard title="Profit" />
+        {sortedTasks.map((task) => (<TaskSummaryCard title={task.title} key={task.title} />))}
       </TaskSummaryCardContainer>
     </div>
   );

45eb1dd1ff1d4461b056a8f6b4c063a94bc8ed80
Garrett DeMeyer <gddemeyer@gmail.com>
2026-02-21 22:52:25 -0600

    feat: update on mobile

--- src/index.tsx
+++ src/index.tsx
@@ -20,7 +20,20 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+// When a new service worker is waiting, tell it to skip waiting then reload
+// so mobile clients pick up new deployments without requiring a tab close.
+serviceWorkerRegistration.register({
+  onUpdate: (registration) => {
+    const waiting = registration.waiting;
+    if (waiting) {
+      // Once the new SW takes control, reload to serve the fresh assets.
+      navigator.serviceWorker.addEventListener('controllerchange', () => {
+        window.location.reload();
+      });
+      waiting.postMessage({ type: 'SKIP_WAITING' });
+    }
+  },
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
